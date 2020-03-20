6fa39dcc3bf782e422a52449a6a4003f66cdbeef
Tanya Kl <tatiana.klenk@gmail.com>
2020-03-20 13:51:04 +0100

    issue5: add autocomplete to input in Ingredient component

--- src/containers/Content/Ingredient.js
+++ src/containers/Content/Ingredient.js
@@ -1,39 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
+
 import img3 from '../../components/Images/cosmetic-shop-01.jpg';
 import { Header, Title, Text, Results } from '../../components/Layout';
+import Autocomplete from '../../components/Autocomplete';
 
-const Input = styled.input`
-  color: white;
-  border-width: 10px;
-  border-color: rgba(47, 56, 84, 0);
-  border-radius: 60px;
-  letter-spacing: 1px;
-  font-size: 16px;
-  text-transform: uppercase;
-  background-color: #2f3854;
-  padding: 0.3em 1em;
-  width: 45%;
-  margin: 1rem 0;
+const AutocompleteWrapper = styled.div`
+  position: relative;
+  display: inline-block;
 `;
 
-const Ingredient = () => {
-  return (
-    <div>
-      <Header img={img3}>
-        <div>
-          <Title>Find Ingredient</Title>
-          <Text>
-            Start typing ingredient your are interested in and check out!
-          </Text>
-        </div>
-        <div>
-          <Input type='text' placeholder='Start typing' />
-        </div>
-      </Header>
-      <Results>tu bedzie komponent WYNIK WYSZUKIWANIA SKLADNIKA</Results>
-    </div>
-  );
-};
+class Ingredient extends Component {
+  state = { val: '' };
+
+  render() {
+    return (
+      <div>
+        <Header img={img3}>
+          <div>
+            <Title>Find Ingredient</Title>
+            <Text>
+              Start typing ingredient your are interested in and check out!
+            </Text>
+          </div>
+          <AutocompleteWrapper>
+            <Autocomplete
+              suggestions={[
+                'Sodium Lauryl Sulphate',
+                'Sodium Lauryl Ether Sulphate',
+                'Sodium Borate',
+                'Tetrasodium EDTA',
+                'Trisodium EDTA',
+                'Glycolic Acid',
+              ]}
+            />
+          </AutocompleteWrapper>
+        </Header>
+        <Results>Tu beda wyniki... beda beda!</Results>
+      </div>
+    );
+  }
+}
 
 export default Ingredient;
